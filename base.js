c8c2b91b8975bdc99350f0fa9ec7a1059e472077
Mohsen Golestan <mgolestan.me@gmail.com>
2024-04-11 17:56:47 -0700

    should start an eslint set of rules with a eslint-config to be able to extend from it in the consumer packages

--- base.js
+++ base.js
@@ -5,10 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-'use strict';
+"use strict";
 
 // Fix eslint shareable config (https://github.com/eslint/eslint/issues/3458)
-require('@rushstack/eslint-patch/modern-module-resolution');
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 // This file contains the minimum ESLint configuration required for Create
 // React App support, and is used as the `baseConfig` for `eslint-loader`
@@ -17,34 +17,34 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 module.exports = {
   root: true,
 
-  parser: '@babel/eslint-parser',
+  parser: "@babel/eslint-parser",
 
-  plugins: ['react'],
+  plugins: ["react"],
 
   env: {
     browser: true,
     commonjs: true,
     es6: true,
     jest: true,
-    node: true,
+    node: true
   },
 
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module",
     requireConfigFile: false,
     babelOptions: {
-      presets: [require.resolve('babel-preset-react-app/prod')],
-    },
+      presets: [require.resolve("babel-preset-react-app/prod")]
+    }
   },
 
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: "detect"
+    }
   },
 
   rules: {
-    'react/jsx-uses-vars': 'warn',
-    'react/jsx-uses-react': 'warn',
-  },
+    "react/jsx-uses-vars": "warn",
+    "react/jsx-uses-react": "warn"
+  }
 };
